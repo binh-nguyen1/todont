ad6b4bed05b189122dae69685799e72d9fc73b7c
Ubuntu <binhnguyen@firstTry.kqfopvxajkcu5opusfdbpzvjkc.gx.internal.cloudapp.net>
2020-02-19 15:56:16 +0000

    some mssage

--- index.js
+++ index.js
@@ -5,10 +5,32 @@ const path    = require('path');
 // Gives direct access to GET files from the
 // "public" directory (you can name the directory anything)
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 // Default route
 app.get('/', (req, res) => {
+    console.log("GET [/]");
+    let data = {
+        query : req.query,
+        body : req.body,
+        params : req. params,
+    }; 
     res.send('Hello, World!');
+});
+
+app.post('/html/todont.html', (req, res) => {
+    console.log("POST [/html/todont/html]");
+    let data = {
+        query : req.query,
+        body : req.body,
+        params : req. params,
+    };
+    console.log(data);
+    res.send(JSON.stringify(data));
+});
+
+app.get("/todont_list", (req,res) => {
+    res.sendFile( path.join(__dirname, "/public/html/todont.html"));
 });
 
 // Listen on port 80 (Default HTTP port)
